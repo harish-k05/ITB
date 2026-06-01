1be2d3d7a1705a1d08d4a687231d9a1edf646dec
harish_k05 <harishkece2026@gmail.com>
2026-06-01 22:35:29 +0530

    second commit

---
+++ backend/src/server.js
@@ -0,0 +1,26 @@
+import dotenv from "dotenv";
+import app from "./app.js";
+import connectDB from "./config/database.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.on("error", (error) => {
+      console.log("error", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is Running on ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log("MongoDB Connection ERROR :", error);
+  }
+};
+
+startServer();
